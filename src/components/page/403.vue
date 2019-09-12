2399606adde49931509dfd685425e14ca9fb1dde
yangfeng07 <2102907006@qq.com>
2019-09-12 15:19:49 +0800

    test1

--- src/components/page/403.vue
+++ src/components/page/403.vue
@@ -4,7 +4,7 @@
       <div class="error-desc">啊哦~ 你没有权限访问该页面哦</div>
       <div class="error-handle">
           <router-link to="/">
-            <el-button type="primary" size="large">返回首页</el-button>
+            <el-button type="primary" size="large">返回首页11</el-button>
           </router-link>
           <el-button class="error-btn" type="primary" size="large" @click="goBack">返回上一页</el-button>
       </div>
